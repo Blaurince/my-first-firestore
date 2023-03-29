0a03383f0b244474d69d7541de3703701b774332
Barbara Laurince <blaurince@hotmail.com>
2023-03-29 15:50:22 -0400

    more firestore practice

--- index.js
+++ index.js
@@ -23,11 +23,45 @@ const candy2 = {
     productNumber:2,
 }
 
-db.collection("products").add(candy2)  // while we are waiting for the promise...
-    .then ((doc) => {
-    console.log("added doc: " + doc.id) // I can be sure that the process was completed successfully
-    return db.collection('products').get() // also returns a promise
+// // How to add a document to firestore
+// db.collection("products").add(candy2)  // while we are waiting for the promise...
+//     .then ((doc) => {
+//     console.log("added doc: " + doc.id) 
+//     // I can be sure that the process was completed successfully
+    
+// })
+
+// .catch(err => console.log(err))
+
+// how to read a document from firestore:
+
+// how to update a document
+db.collection('products').doc('V3kDn9dSCyYeBVHom5fC').update({
+     inventory:555,
+     customerFavorite: true 
+
 })
-.then()
+
+
+
+
+// how to read 
+db.collection('products').doc('V3kDn9dSCyYeBVHom5fC').get()
+   .then(doc => {
+     console.log(doc.data())
+})
 .catch(err => console.log(err))
+
+// how to get a whole collection
+
+db.collection('products').get()
+.then(collection => {
+    const productList = collection.docs.map(doc => ({...doc.data(), id: doc.id}));
+    console.table(productList);
+})
+.catch(console.log);
+
+// how to delete a document
+
+//db.collection('products').doc('V3kDn9dSCyYeBVHom5fC').delete()
 
